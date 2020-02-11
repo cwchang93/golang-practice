67a9d00312704314bbba1e9c796a0ed33dbde8ac
Jinwei <cwchang93@gmail.com>
2020-02-11 18:32:40 +0800

    Add q1

---
+++ q1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("hi")
+	// fmt.Print(q1())
+	// num := 123
+	// var numofzero = 5 - len(strconv.Itoa(num))
+	// fmt.Printf("0"*numofzero + %d\n", 123)
+	// fmt.Printf("0" * numofzero)
+	fmt.Printf("%05d", 12)
+}
+
+func q1() string {
+	total := 100000
+	num := 12
+
+	// var lenofNum = len(strconv.Itoa(num))
+	var final = strconv.Itoa(total + num)
+
+	// println(len(strconv.Itoa(num)))
+	// strconv.Itoa(final)
+	return final[1:]
+
+}
---
+++ syntax.md
@@ -0,0 +1,47 @@
+#### import 語法
+```go
+package main
+
+import "fmt"
+import "math"
+
+import (
+    "fmt"
+    "math"
+    )
+
+func main() {
+    fmt.Printf
+}
+
+```
+
+#### 大寫字母開頭為export 
+如果要取用package裡面的函數，必須要用大寫
+e.g. math.pi => math.Pi
+
+####　function 宣告
+- 參數與結果都要宣告
+- 相同型別可以省略
+
+```go
+
+func add()
+
+
+```
+
+#### Print
+- fmt.Println => print line
+- fmt.Printf => 第一個參數為string, 
+
+
+#### :=用途為何?
+:=
+
+#### func只要定義就一定會執行?
+
+#### 不能用單引號
+
+- "%T" type
+- 只有var才能 := , const 不能
